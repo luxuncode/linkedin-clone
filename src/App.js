ddca228b4abccd1215bd933a1bf6aeaf6227aece
Agus Riyadi <agus.riyadi.al@gmail.com>
2021-02-15 21:26:39 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './Header';
+import Body from './Body'
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Body />
     </div>
   );
 }
